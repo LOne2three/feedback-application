0e5b0e8363621dd1b1bcdefd705045f232cfb150
Liangyew <wongly280@gmail.com>
2023-07-30 21:31:11 +0100

    Installed json-server and moved placeholder data to json file

--- src/context/FeedbackContext.jsx
+++ src/context/FeedbackContext.jsx
@@ -1,16 +1,23 @@
-import{createContext, useState } from  'react'
+import{createContext, useState, useEffect } from  'react'
 import { v4 as uuidv4} from 'uuid'
 
 const FeedbackContext = createContext()
 
 export const FeedbackProvider = ({children}) => {
-const[feedback,setFeedback] = useState([
-    {
-        id: "1",
-        text: 'This is feedback item 1',
-        rating: 10
-    }
-])
+const[feedback,setFeedback] = useState([])
+const[isLoading, setIsLoading] = useState(true)
+useEffect(() =>{
+  fetchFeedback()
+},[])
+
+const fetchFeedback = async () => {
+  const response = await fetch(`http://localhost:5000/feedback?_sort=id&_order=desc`)
+  const data = await response.json()
+  
+  setFeedback(data)
+  setIsLoading(false)
+
+}
 
 const [feedbackEdit, setFeedbackEdit] = useState({
   item: {},
@@ -41,7 +48,7 @@ const addFeedback = (newFeedback) =>{
 
 return(
 <FeedbackContext.Provider 
-value={{feedback,deleteFeedback,addFeedback,editFeedback, feedbackEdit,updateFeedback}}>
+value={{feedback,deleteFeedback,addFeedback,editFeedback, feedbackEdit,updateFeedback, isLoading}}>
     {children}
 </FeedbackContext.Provider>
 )
